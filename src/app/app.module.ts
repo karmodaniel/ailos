41ae09380641f43e704af8567d8763d19c7e9661
daniel.carmo <daniel.carmo2008@gmail.com>
2023-01-25 02:45:27 -0300

    feat: add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layouts/header/header.component';
+import { NavigationComponent } from './layouts/navigation/navigation.component';
+import { AdmissionCooperativeModule } from './pages/admission-cooperative/admission-cooperative.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AdmissionCooperativeModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBadgeIconDirective } from './directives/mat-badge-icon.directive';
 
-const modules = [MatSidenavModule, MatListModule, MatIconModule];
+const modules = [
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatBadgeModule,
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MatBadgeIconDirective
+  ],
   imports: [CommonModule, ...modules],
-  exports: [...modules],
+  exports: [...modules, MatBadgeIconDirective],
 })
 export class SharedModule {}
