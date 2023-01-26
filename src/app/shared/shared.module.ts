77710ffb0005e2632adb20c268db95b4b7aa317d
daniel.carmo <daniel.carmo2008@gmail.com>
2023-01-26 01:31:51 -0300

    chore: update shared with new imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule } from '@angular/material/badge';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+
 import { MatBadgeIconDirective } from './directives/mat-badge-icon.directive';
+
+
 
 const modules = [
   MatSidenavModule,
@@ -15,6 +20,9 @@ const modules = [
   MatToolbarModule,
   MatButtonModule,
   MatBadgeModule,
+  MatStepperModule,
+  NgxMaskDirective,
+  NgxMaskPipe
 ];
 
 @NgModule({
@@ -23,5 +31,6 @@ const modules = [
   ],
   imports: [CommonModule, ...modules],
   exports: [...modules, MatBadgeIconDirective],
+  providers: [provideNgxMask()]
 })
 export class SharedModule {}
