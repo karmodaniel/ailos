9f747c9b8790e584d25e03887999dff3ad668139
daniel.carmo <daniel.carmo2008@gmail.com>
2023-01-27 15:15:39 -0300

    test: add tests to cpf-service

--- src/app/core/services/cpf-consult/cpf-consult.service.spec.ts
+++ src/app/core/services/cpf-consult/cpf-consult.service.spec.ts
@@ -1,16 +1,48 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { User } from '../../../shared/models/user/user.class';
 import { CpfConsultService } from './cpf-consult.service';
 
-describe('CpfConsultService', () => {
+fdescribe('CpfConsultService', () => {
   let service: CpfConsultService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CpfConsultService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get user by id', () => {
+    const cpf = '02031516201';
+    const response = new User(
+      '02031516201',
+      '020.315.162-01',
+      'DANIEL ARAUJO DO CARMO',
+      '12/01/1998',
+      'Regular',
+      '21/05/2010'
+    );
+
+    service.getUserByCpf(cpf).subscribe((res) => {
+      expect(res).toBe(response);
+    });
+
+    const req = httpController.expectOne(`http://localhost:3000/users/${cpf}`);
+
+    expect(req.request.url).toBe(`http://localhost:3000/users/${cpf}`);
+    expect(req.request.method).toBe('GET');
+
+    req.flush(response);
   });
 });
