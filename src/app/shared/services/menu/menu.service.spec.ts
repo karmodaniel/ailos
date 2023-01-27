5b3946182309af445d0736cc7914d9a6a3ba5111
daniel.carmo <daniel.carmo2008@gmail.com>
2023-01-27 17:07:49 -0300

    test: add menu service tests

--- src/app/shared/services/menu/menu.service.spec.ts
+++ src/app/shared/services/menu/menu.service.spec.ts
@@ -1,16 +1,37 @@
+import { MatSidenav } from '@angular/material/sidenav';
 import { TestBed } from '@angular/core/testing';
 
 import { SidenavService } from './menu.service';
 
 describe('SidenavService', () => {
   let service: SidenavService;
+  let sidenav: MatSidenav;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SidenavService],
+    });
     service = TestBed.inject(SidenavService);
+    sidenav = jasmine.createSpyObj('MatSidenav', ['open', 'close', 'toggle']);
+    service.setSidenav(sidenav);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should open the sidenav', () => {
+    service.open();
+    expect(sidenav.open).toHaveBeenCalled();
+  });
+
+  it('should close the sidenav', () => {
+    service.close();
+    expect(sidenav.close).toHaveBeenCalled();
+  });
+
+  it('should toggle the sidenav', () => {
+    service.toggle();
+    expect(sidenav.toggle).toHaveBeenCalled();
   });
 });
