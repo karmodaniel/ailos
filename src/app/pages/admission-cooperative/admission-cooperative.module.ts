ded34ac9be54270f705e6e6c379cf51f84bdb292
daniel.carmo <daniel.carmo2008@gmail.com>
2023-01-27 00:47:58 -0300

    style: update import paths

--- src/app/pages/admission-cooperative/admission-cooperative.module.ts
+++ src/app/pages/admission-cooperative/admission-cooperative.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CpfConsultService } from 'src/app/core/services/cpf-consult/cpf-consult.service';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { CpfConsultService } from '@core/services/cpf-consult/cpf-consult.service';
+import { SharedModule } from '@shared/shared.module';
 
 import { AdmissionCooperativeRoutingModule } from './admission-cooperative-routing.module';
 import { AdmissionCooperativeComponent } from './admission-cooperative.component';
